3083018e1a99d5c970e31ef1948476fcfcfa2f79
Jake Mannix <jakem@allenai.com>
2015-08-13 16:50:26 -0700

    Get some white lines!

--- src/main/scala/org/allenai/pdfsubfigures/dissect/PngDissector.scala
+++ src/main/scala/org/allenai/pdfsubfigures/dissect/PngDissector.scala
@@ -1,22 +1,27 @@
 package org.allenai.pdfsubfigures.dissect
 
+import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
+import scala.collection.immutable.IndexedSeq
 
-class PngDissector(img: BufferedImage) {
 
+class PngDissector(val img: BufferedImage) {
   /**
-   * Writes a black line horizontally across the image, and returns the modified image
+   * Writes a  line horizontally across the image, and returns the modified image
    * @param y height at which to draw the line
    * @param xStart
    * @param xEnd
+   * @rgb color
    * @return
    */
-  def addHorizBlackLine(y: Int, xStart: Int = 0, xEnd: Int = img.getWidth - 1) = {
+  def addHorizLine(y: Int, xStart: Int = 0, xEnd: Int = img.getWidth - 1,
+      rgb: Color = Color.BLACK) = {
     for (x <- xStart until xEnd) {
-      img.setRGB(x, y, 0)
+      img.setRGB(x, y,
+        (rgb.getAlpha() << 24) | (rgb.getRed() << 16) | (rgb.getGreen() << 8) | (rgb.getBlue() << 0))
     }
     img
   }
@@ -28,19 +33,83 @@ class PngDissector(img: BufferedImage) {
    * @param yEnd
    * @return
    */
-  def addVertBlackLine(x: Int, yStart: Int = 0, yEnd: Int = img.getHeight - 1) = {
+  def addVertLine(x: Int, yStart: Int = 0, yEnd: Int = img.getHeight - 1,
+      rgb: Color = Color.BLACK) = {
     for (y <- yStart until yEnd) {
-      img.setRGB(x, y, 0)
+      img.setRGB(x, y,
+        (rgb.getAlpha() << 24) | (rgb.getRed() << 16) | (rgb.getGreen() << 8) | (rgb.getBlue() << 0))
     }
     img
   }
+
+  def toMatrix(xStart: Int = 0,
+      yStart: Int = 0,
+      xEnd: Int = img.getWidth - 1,
+      yEnd: Int = img.getHeight - 1): IndexedSeq[(Int, Int, Color)] = {
+    for { x <- xStart until xEnd
+          y <- yStart until yEnd } yield {
+      (x, y, new Color(img.getRGB(x, y)))
+    }
+  }
+
+  def findWhiteColumns(xStart: Int = 0,
+      yStart: Int = 0,
+      xEnd: Int = img.getWidth - 1,
+      yEnd: Int = img.getHeight - 1): List[Int] =
+    (xStart until xEnd).filter(x => (yStart until yEnd).forall(y =>
+      isWhite(new Color(img.getRGB(x, y))))).toList
+
+  def findWhiteRows(xStart: Int = 0,
+      yStart: Int = 0,
+      xEnd: Int = img.getWidth - 1,
+      yEnd: Int = img.getHeight - 1): List[Int] =
+    (yStart until yEnd).filter(y => (xStart until xEnd).forall(x =>
+      isWhite(new Color(img.getRGB(x, y))))).toList
+
+  def findWhiteColumnEdges(xStart: Int = 0,
+      yStart: Int = 0,
+      xEnd: Int = img.getWidth - 1,
+      yEnd: Int = img.getHeight - 1): List[Int] = {
+    ???
+  }
+
+  def reddenWhiteColumns(xStart: Int = 0,
+      yStart: Int = 0, xEnd: Int = img.getWidth - 1, yEnd: Int = img.getHeight - 1) = {
+    findWhiteColumns(xStart, yStart, xEnd, yEnd).foreach { x =>
+      addVertLine(x, yStart, yEnd, Color.RED)
+    }
+    new PngDissector(img)
+  }
+
+  def reddenWhiteRows(xStart: Int = 0,
+      yStart: Int = 0, xEnd: Int = img.getWidth - 1, yEnd: Int = img.getHeight - 1) = {
+    findWhiteRows(xStart, yStart, xEnd, yEnd).foreach { y =>
+      addHorizLine(y, xStart, xEnd, Color.RED)
+    }
+    new PngDissector(img)
+  }
+
+  def write(outFileName: String): Unit = {
+    ImageIO.write(img, "png", new File(outFileName))
+  }
+
+  def isWhite(rgba: Color, ignoreRed: Boolean = true): Boolean = {
+    //(ignoreRed || asColor.getRed == 255) &&
+    rgba == Color.RED ||
+        (rgba.getGreen == 255 && rgba.getBlue == 255)
+  }
 }
 
-object PngDissector extends App {
+object PngDissector {
+  def apply(fileName: String) = new PngDissector(ImageIO.read(new File(fileName)))
+}
+
+object PngDissectorApp extends App {
   val fileName = args(0)
-  val lineY = args(1).toInt
-  val outFileName = args(2)
-  val pngEditor = new PngDissector(ImageIO.read(new File(fileName)))
-  val outImg = pngEditor.addHorizBlackLine(lineY)
-  ImageIO.write(outImg, "png", new File(outFileName))
+  val outFileName = fileName.replace("png", "redlines.png")
+  val pngDissector = new PngDissector(ImageIO.read(new File(fileName)))
+  val whiteColumns = pngDissector.findWhiteColumns()
+  val whiteRows = pngDissector.findWhiteRows()
+  val outImg = pngDissector.reddenWhiteColumns().reddenWhiteRows()
+  ImageIO.write(outImg.img, "png", new File(outFileName))
 }
