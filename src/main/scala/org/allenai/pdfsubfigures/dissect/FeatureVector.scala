859d45586edaba82afdc58b64b5c929d961b890b
Roie Levin <roiel@allenai.org>
2015-08-13 21:42:55 -0700

    modifying feature vector

--- src/main/scala/org/allenai/pdfsubfigures/dissect/FeatureVector.scala
+++ src/main/scala/org/allenai/pdfsubfigures/dissect/FeatureVector.scala
@@ -11,7 +11,8 @@ class FeatureVector(val split: Split, val box: Box) {
                   else {Box(box.xStart, box.xEnd, split.end, box.yEnd)}
 
   val width = split.width.toDouble
-  val aspectRatio = boxChild1.height.toDouble/boxChild1.width.toDouble
+  val aspectRatio = math.max(math.abs(math.log(boxChild1.height.toDouble/boxChild1.width.toDouble)),
+    math.abs(math.log(boxChild2.height.toDouble/boxChild2.width.toDouble)))
   val blankCoverage = split.width.toDouble / (if (split.isVertical) box.width.toDouble else box.height.toDouble)
   val absoluteArea = math.min(boxChild1.width*boxChild1.height.toDouble, boxChild2.width*boxChild2.height.toDouble)
   val smallestDimension = math.min(math.min(boxChild1.height, boxChild1.width),math.min(boxChild2.height, boxChild2.width))
@@ -26,7 +27,6 @@ class FeatureVector(val split: Split, val box: Box) {
     val aspectRatioWeight = 0
     val blankCoverageWeight = 0
 
-    return (if (smallestDimension > 20) 1.0 else 0.0)*
-      widthWeight*width.toDouble
+    return if (smallestDimension > 20 && aspectRatio < 0.47) widthWeight*width.toDouble else 0
   }
 }
